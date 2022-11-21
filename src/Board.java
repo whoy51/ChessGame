49f57e6d21561cc74341bac2c1e9aa20c8e3dca3
Boqueronix <66971655+Boqueronix@users.noreply.github.com>
2022-11-21 13:49:14 -0500

    More Typing

--- src/Board.java
+++ src/Board.java
@@ -7,7 +7,7 @@ public class Board{
                 double y = j / 8.0;
                 StdDraw.setPenColor(((i + j) % 2 == 0)? Color.WHITE : Color.BLACK);
                 StdDraw.filledSquare((x + 1 / 16.0), (y + 1 / 16.0), (1 / 16.0));
-                //System.out.println("Made square at column " + i + " and row " + j);
+                System.out.println("Made square at column " + i + " and row " + j);
             }
         }
     }
