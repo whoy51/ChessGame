4bee6f1387bc23ccf371453dfd9614b3c93ce7c0
Boqueronix <66971655+Boqueronix@users.noreply.github.com>
2022-11-21 22:26:34 -0500

    Update Board.java
    Changed Colors to visible with WB pieces and created a coords array

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,27 @@
 import java.awt.Color;
-public class Board{
-    public static void main (String[] args){
+public class Board {
+    public static double[][][] boardCoords= new double[8][8][2];
+    public static void fillCoords(){
         for (int i = 0; i < 8; i++){
-            double x = i / 8.0;
             for (int j = 0; j < 8; j++){
-                double y = j / 8.0;
-                StdDraw.setPenColor(((i + j) % 2 == 0)? Color.WHITE : Color.BLACK);
-                StdDraw.filledSquare((x + 1 / 16.0), (y + 1 / 16.0), (1 / 16.0));
-                System.out.println("Made square at column " + i + " and row " + j);
+                boardCoords[i][j][0] = i / 8.0 + 1/16.0;
+                //System.out.println(boardCoords[i][j][0]);
+                boardCoords[i][j][1] = j / 8.0 + 1/16.0;
+                //System.out.println(boardCoords[i][j][1]);
+            }
+        }
+    }
+    public static void init() {
+        Board.fillCoords();
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                if ((i + j) % 2 == 0){
+                    StdDraw.setPenColor(new Color(128, 64, 0));
+                } else {
+                    StdDraw.setPenColor(new Color(255, 204, 153));
+                }
+                StdDraw.filledSquare(boardCoords[i][j][0], boardCoords[i][j][1], 1/16.0);
+                //System.out.println("Made square at column " + i + " and row " + j);
             }
         }
     }
