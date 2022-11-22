9e4d95ae3174a6ac2abbe0aae5c9b7847ecd4dd7
Boqueronix <66971655+Boqueronix@users.noreply.github.com>
2022-11-21 23:20:12 -0500

    Piece as parent

--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,16 @@
-public class Piece {
+package Chess;
 
-// What is piece
+public class Piece {
+    public int[] coords = new int[2];
+    public boolean eaten = false;
+    public boolean white;
+    public void draw(){
+
+    }
+    public Piece(int row, int col, boolean whi){
+        coords[0] = row - 1;
+        coords[1] = col - 1;
+        white = whi;
+        this.draw();
+    }
 }
