c94033565f3c9a0f97d1ea0e38c3b4219e7428d5
Wesley Hoy <whoy@ey3.tech>
2022-11-22 10:46:11 -0500

    Add pictures for king and queen

--- src/King.java
+++ src/King.java
@@ -11,6 +11,10 @@ public class King extends Piece{
         } else {
             StdDraw.setPenColor(Color.BLACK);
         }
-        StdDraw.text(coords[1] / 8.0 + 1 / 16.0, coords[0] / 8.0 + 1 / 16.0, "K");
+        if (StdDraw.getPenColor() == Color.WHITE) {
+            StdDraw.picture(coords[1] / 8.0 + 1 / 16.0, coords[0] / 8.0 + 1 / 16.0, "/resources/king_white.png", 0.1, 0.1);
+        } else {
+            StdDraw.picture(coords[1] / 8.0 + 1 / 16.0, coords[0] / 8.0 + 1 / 16.0, "/resources/king_black.png", 0.1, 0.1);
+        }
     }
 }
