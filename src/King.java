2b64ad54a43e46fea0c956ffb9adccf4be9ec5ff
Boqueronix <66971655+Boqueronix@users.noreply.github.com>
2022-11-22 09:30:19 -0500

    Added Draw method

--- src/King.java
+++ src/King.java
@@ -3,5 +3,12 @@ public class King extends Piece{
     public King(int row, int col, boolean whi) {
         super(row, col, whi);
     }
-    
+    public void draw(){
+        if (white){
+            StdDraw.setPenColor(Color.WHITE);
+        } else {
+            StdDraw.setPenColor(Color.BLACK);
+        }
+        StdDraw.text(coords[1] / 8.0 + 1 / 16.0, coords[0] / 8.0 + 1 / 16.0, "K");
+    }
 }
